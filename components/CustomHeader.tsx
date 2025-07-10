3605d9c37096aef6b965507f7eaec69e3f286894
Khoa1512 <danggkhoaa1512@gmail.com>
2025-07-10 16:19:44 +0700

    Implement cart screen

--- components/CustomHeader.tsx
+++ components/CustomHeader.tsx
@@ -1,15 +1,27 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { CustomHeaderProps } from '@/type'
+import { useRouter } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 
-const CustomHeader = ({title}: CustomHeaderProps) => {
+import { CustomHeaderProps } from "@/type";
+import { images } from "@/constants";
+
+const CustomHeader = ({ title }: CustomHeaderProps) => {
+  const router = useRouter();
+
   return (
     <View className='custom-header'>
-      <TouchableOpacity>
-        
+      <TouchableOpacity onPress={() => router.back()}>
+        <Image
+          source={images.arrowBack}
+          className='size-5'
+          resizeMode='contain'
+        />
       </TouchableOpacity>
-    </View>
-  )
-}
 
-export default CustomHeader
+      {title && <Text className='base-semibold text-dark-100'>{title}</Text>}
+
+      <Image source={images.search} className='size-5' resizeMode='contain' />
+    </View>
+  );
+};
+
+export default CustomHeader;
--- components/MenuCart.tsx
+++ components/MenuCart.tsx
@@ -1,9 +1,11 @@
 import { appwriteConfig } from "@/lib/appwrite";
+import { useCartStore } from '@/store/cart.store';
 import { MenuItem } from "@/type";
 import { Image, Platform, Text, TouchableOpacity } from "react-native";
 
-const MenuCart = ({ item: { image_url, name, price } }: { item: MenuItem }) => {
+const MenuCart = ({ item: { $id, image_url, name, price } }: { item: MenuItem }) => {
   const imageUrl = `${image_url}?project=${appwriteConfig.projectId}`;
+  const {addItem} = useCartStore();
   return (
     <TouchableOpacity className='menu-card' style={Platform.OS === 'android' ? {elevation: 10, shadowColor: '#878787'} : {}}>
       <Image
@@ -18,7 +20,7 @@ const MenuCart = ({ item: { image_url, name, price } }: { item: MenuItem }) => {
         {name}
       </Text>
       <Text className='body-regular text-gray-200 mb-4'>From ${price}</Text>
-      <TouchableOpacity onPress={() => {}}>
+      <TouchableOpacity onPress={() => addItem({id: $id, name, price, image_url: image_url, customizations: [] })}>
         <Text className='paragraph-bold text-primary'>Add to Cart +</Text>
       </TouchableOpacity>
     </TouchableOpacity>
